6e015253e84b8a9e893fb4cce746cfb18beae340
Gideon Peters <my@pers.on.al>
2022-08-01 23:22:44 +0100

    style: format files using prettier config

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,36 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routeEndpoints = {
-  HOME: {
-    name: "home",
-    path: "/",
-  },
-  DOG_VIEW: {
-    name: "dogs.view",
-    path: "/dogs/:id",
-  },
+	HOME: {
+		name: 'home',
+		path: '/',
+	},
+	DOG_VIEW: {
+		name: 'dogs.view',
+		path: '/dogs/:id',
+	},
 };
 
 const routes = [
-  {
-    path: routeEndpoints.HOME.path,
-    name: routeEndpoints.HOME.name,
-    component: () =>
-      import(/* webpackChunkName: "dogs" */ "../views/HomeView.vue"),
-  },
-  {
-    path: routeEndpoints.DOG_VIEW.path,
-    name: routeEndpoints.DOG_VIEW.name,
-    component: () =>
-      import(/* webpackChunkName: "dogs" */ "../views/DogView.vue"),
-  },
+	{
+		path: routeEndpoints.HOME.path,
+		name: routeEndpoints.HOME.name,
+		component: () => import(/* webpackChunkName: "dogs" */ '../views/HomeView.vue'),
+	},
+	{
+		path: routeEndpoints.DOG_VIEW.path,
+		name: routeEndpoints.DOG_VIEW.name,
+		component: () => import(/* webpackChunkName: "dogs" */ '../views/DogView.vue'),
+	},
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes,
 });
 
 export default router;
