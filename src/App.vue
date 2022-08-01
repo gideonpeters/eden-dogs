49ec5ea780fe9d3fc77b135d5bfc46e3fbb6c011
Gideon Peters <my@pers.on.al>
2022-08-01 23:23:28 +0100

    feat: add SiteLayout component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
-	<div id="app" class="relative py-3 px-10 bg-green-30 flex flex-col min-h-screen h-full overflow-x-hidden">
-		<SiteHeader class="z-40" />
-
+	<SiteLayout>
 		<router-view class="z-40" />
-
-		<svg class="absolute z-0 top-[-2rem] left-[-5rem] h-[30rem] w-[30rem] text-white">
-			<use href="@/assets/img/blob-1.svg#blob-1"></use>
-		</svg>
-		<svg class="absolute z-0 top-[-2rem] right-[-5rem] h-[30rem] w-[30rem] text-white">
-			<use href="@/assets/img/blob-3.svg#blob-3"></use>
-		</svg>
-		<svg class="absolute z-0 bottom-[-2rem] right-[-5rem] h-[30rem] w-[30rem] text-white">
-			<use href="@/assets/img/blob-2.svg#blob-2"></use>
-		</svg>
-	</div>
+	</SiteLayout>
 </template>
 
 <script>
-import SiteHeader from '@/components/SiteHeader.vue';
+import SiteLayout from '@/components/layouts/SiteLayout.vue';
 
 export default {
 	components: {
-		SiteHeader,
+		SiteLayout,
 	},
 };
 </script>
---
+++ src/components/layouts/SiteLayout.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app" class="relative py-3 px-32 bg-green-10 flex flex-col min-h-screen h-full overflow-x-hidden">
+		<SiteHeader class="z-40" />
+
+		<slot />
+
+		<svg class="absolute z-0 top-[-2rem] left-[-5rem] h-[30rem] w-[30rem] text-purple">
+			<use href="@/assets/img/blob-1.svg#blob-1"></use>
+		</svg>
+		<svg class="absolute z-0 top-[-2rem] right-[3rem] h-[30rem] w-[30rem] text-purple">
+			<use href="@/assets/img/blob-3.svg#blob-3"></use>
+		</svg>
+		<svg class="absolute z-0 bottom-[-2rem] right-[3rem] h-[30rem] w-[30rem] text-purple">
+			<use href="@/assets/img/blob-2.svg#blob-2"></use>
+		</svg>
+	</div>
+</template>
+
+<script>
+import SiteHeader from '@/components/SiteHeader.vue';
+
+export default {
+	components: {
+		SiteHeader,
+	},
+};
+</script>
