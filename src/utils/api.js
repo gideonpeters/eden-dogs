98323c2d649511bf73bdfd9b6036259d9034664b
Gideon Peters <my@pers.on.al>
2022-08-02 15:58:21 +0100

    fix: remove interceptor causing inifinite loading

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,4 @@
-import store from '@/store';
 import axios from 'axios';
-
-axios.interceptors.response.use(
-	(response) => {
-		return response;
-	},
-	(error) => {
-		store.commit('TOGGLE_SNACKBAR', error);
-		return Promise.reject(error);
-	}
-);
 
 export const http = axios.create({
 	baseURL: process.env.VUE_APP_API_BASE,
