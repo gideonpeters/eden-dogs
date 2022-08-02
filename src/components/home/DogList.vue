d4809f1d12e3836c7840d555f3781418286a3736
Gideon Peters <my@pers.on.al>
2022-08-02 02:56:34 +0100

    feat: add utility classes for responsiveness

--- src/components/home/DogList.vue
+++ src/components/home/DogList.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="grid grid-cols-4 gap-4 mt-4">
-		<DogCard v-for="(dog, ix) in dogs" :key="ix" :image="dog" />
+	<div class="grid grid-cols-1 md:grid-cols-4 gap-4 mt-5 place-items-center">
+		<DogCard v-for="(dog, ix) in dogs" :key="ix" :image="dog" class="self-center" />
 	</div>
 </template>
 
--- src/components/home/HeroSection.vue
+++ src/components/home/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="flex justify-between items-center">
-		<div class="sm:text-center md:max-w-2xl md:mx-auto lg:w-1/2 lg:text-left">
+	<div class="flex md:flex-row flex-col justify-between items-center">
+		<div class="sm:text-center w-full md:w-7/12 lg:text-left">
 			<h1>
 				<span class="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
 					<span class="block text-black">
@@ -18,13 +18,17 @@
 				Search for your desired dog with us. Our website is guaranteed to help you find the pet you would absolutely love.
 			</p>
 		</div>
-		<div class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:w-1/2 lg:flex lg:items-center">
-			<div class="relative mx-auto w-full lg:max-w-md">
+		<div class="md:mt-12 mt-5 relative lg:flex lg:items-center w-full md:w-1/3">
+			<div class="relative mx-auto w-full">
 				<div
 					class="relative block w-full rounded-lg shadow-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 				>
 					<span class="sr-only">Use the search input to find dog breeds</span>
-					<img class="w-full object-contain h-[25rem]" src="https://images.dog.ceo/breeds/hound-plott/hhh_plott002.JPG" alt="" />
+					<img
+						class="w-[100%] object-cover object-top h-[25rem]"
+						src="https://images.dog.ceo/breeds/hound-plott/hhh_plott002.JPG"
+						alt=""
+					/>
 				</div>
 			</div>
 		</div>
